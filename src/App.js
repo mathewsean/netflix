48d9b2982abee59dcd29de2e5f38453e09631b91
Shone Antony <shonantony@gmail.com>
2023-07-04 15:46:51 +0100

    uploadToGithub

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import NavBar from './Components/NavBar/NavBar'
+import Banner from './Components/Banner/Banner'
+import './App.css'
+import RowPost from "./Components/RowPost/RowPost";
+import {API_KEY} from "./constants/constants"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Banner />
+      <RowPost title="Originals" url={`/discover/tv?api_key=${API_KEY}&with_networks=213`}/>
+      <RowPost title="Action" isSmall url={`/discover/movie?api_key=${API_KEY}&with_genres=28`}/>
+      <RowPost title="Trending" isSmall url={`/trending/all/week?api_key=${API_KEY}&language=en-US'`} />
+      <RowPost title="Comedy" isSmall url={`/discover/movie?api_key=${API_KEY}&with_genres=35`}/>
+      <RowPost title="Horror" isSmall url={`/discover/movie?api_key=${API_KEY}&with_genres=27`} />  
+
     </div>
   );
 }
